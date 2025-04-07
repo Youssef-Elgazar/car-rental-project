ff34fa76bbbd921a5e4288ebaf6dbceb7060df1d
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2025-04-07 21:14:56 +0200

    First iteration of Search-Area.

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,47 @@ import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 
-
 export function App() {
-  return (
+	return (
+		<div id="hero">
+			<nav>
+				<div id="logo">WHEELZ</div>
+				<ul id="nav-menu">
+					<li>Home</li>
+					<li>Listings</li>
+					<li>Contact</li>
+					<li>About</li>
+					<li>
+						{" "}
+						<FontAwesomeIcon icon={faUser} /> Sign In{" "}
+					</li>
+					<button id="submit-listing-btn">Submit Listing</button>
+				</ul>
+			</nav>
 
-    <div id="hero">
-      <nav>
-        <div id="logo">WHEELZ</div>
-        <ul id="nav-menu">
-          <li>Home</li>
-          <li>Listings</li>
-          <li>Contact</li>
-          <li>About</li>
-          <li> <FontAwesomeIcon icon={faUser} />{" "}Sign In </li>
-          <button id="submit-listing-btn">Submit Listing</button>
+			<div id="search-area">
+				<h2 id="search-heading">FIND YOUR PERFECT CAR</h2>
+				<div className="search-bar">
+					<select className="dropdown">
+						<option>Any Makes</option>
+					</select>
 
-        </ul>
-      </nav>
-    </div>
-  );
+					<select className="dropdown">
+						<option>Any Models</option>
+					</select>
+
+					<div className="prices">
+						<label htmlFor="price">Prices:</label>
+						<select id="price" className="dropdown no-border">
+							<option>All Prices</option>
+						</select>
+					</div>
+
+					<button className="search-button">🔍 Search Cars</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
